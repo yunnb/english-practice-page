66d894df4393774c4ea0e2dfa39d324ed53ebdca
yunnb <chldbsrud010@naver.com>
2024-07-27 22:16:24 +0900

    update tooltip on activity-calendar

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,9 +1,19 @@
 import ActivityCalendar from "react-activity-calendar";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import styled from "styled-components";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+
+const CalenderWrapper = styled.div`
+    margin-top: 100px;
+`;
+
 
 function Calender() {
     const [dates, setDates] = useState([]);
+    const [counts, setCounts] = useState([]);
+
     useEffect(() => {
         axios.get('http://localhost:3001/activity')
             .then(response => {
@@ -12,20 +22,40 @@ function Calender() {
                     count: item.add_count + item.review_count,
                     level: item.level
                 }));
+
+                const newCounts = response.data.map(item => ({
+                    add_count: item.add_count,
+                    review_count: item.review_count
+                }))
                 console.log(newDates);
                 setDates(prevDates => [...prevDates, ...newDates]);
                 // prevDates 는 'dates' 의 값. 초기 상태에서는 빈 배열 -> 초기에는 newDates 만 포함된 새로운 배열이 됨
-                // 만약
+                setCounts(prevCounts => [...prevCounts, ...newCounts]);
             })
             .catch(error => {
                 console.error('Error fetching activities; ', error);
             });
     }, []);
 
+    const tooltipMessage = () => {
+
+
+    }
+
     return (
-        <>
-            <ActivityCalendar data={dates} />
-        </>
+        <CalenderWrapper>
+            <ActivityCalendar
+                data={dates}
+                renderBlock={(block, activity) =>
+                    React.cloneElement(block, {
+                        'data-tooltip-id': 'react-tooltip',
+                        'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
+                    })
+                }
+            />
+
+            <ReactTooltip id="react-tooltip" />
+        </CalenderWrapper>
     );
 }
 
