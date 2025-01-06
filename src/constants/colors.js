897a6fae0cb873e70f0d3a92ac1d7ede6b5bc75d
yunnb <chldbsrud010@naver.com>
2025-01-06 23:12:59 +0900

    feat: color 상수 추가

---
+++ src/constants/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+    primary: {
+        main: '#246BEB'
+    },
+
+    secondary: {
+        5: '#EDF1F5'
+    },
+
+    background: {
+        white: '#FFFFFF',
+        gray: '#F8F9FA'
+    },
+}
